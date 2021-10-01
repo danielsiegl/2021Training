292210c35abd1a1827a254c7860cdeae8dd4f507
dsiegl <daniel.siegl@lieberlieber.com>
2021-10-01 08:58:36 +0200

    Squashed commit of the following:
    commit c23d6b14f541da042fbf0f9c300701576d35b16d
    Author: dsiegl <daniel.siegl@lieberlieber.com>
    Date:   Fri Oct 1 08:58:11 2021 +0200
    
        Changed PWC
    
    commit 41671b1c49a135f8860cbf995e8383dfb8933c9e
    Author: dsiegl <daniel.siegl@lieberlieber.com>
    Date:   Fri Oct 1 08:42:26 2021 +0200
    
        Fixed Use Case

---
+++ src/InjectGuidinXMIRootPackage/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace InjectGuidinXMIRootPackage
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            //my.xmi EAPK_704B4B1E-D532-4C19-866D-77B73E4F1435
+            Console.WriteLine("InjectGuidinXMIRootPackage is starting");
+            try
+            {
+                if (args.Length != 2)
+                {
+                    Console.WriteLine($"Wrong number of commandline parameters! (1) e.g.:  PWC.eapx");
+                    return -1;
+                }
+
+                string filename = args[0];
+                if (!(File.Exists(filename)))
+                {
+                    Console.WriteLine($"{filename} doesn't exist!");
+                    return -2;
+                }
+
+                string newGuid = args[1];
+
+                string text;
+                XDocument srcTree;
+                Console.WriteLine($"Modifying {filename} with {newGuid}");
+
+              
+                srcTree = new XDocument();
+                srcTree = XDocument.Load(filename);
+                var query = from c in srcTree.Elements("xmi:XMI") select c; ;//.Elements("Model").Elements("packagedElement") select c;
+                foreach (var foo in query)
+                {
+                    foo.ToString();
+                }
+                //XmlDocument xmldoc = new XmlDocument();
+                //xmldoc.LoadXml(text);
+                //XmlElement node1 = xmldoc.SelectSingleNode($"/XMI/Model") as XmlElement;
+                //if (node1 != null)
+                //{
+                //    //
+                //}
+                //else
+                //{
+                //    return 8;
+                //}
+
+
+
+            
+
+                using (var stream = File.Open(filename, FileMode.Truncate))
+            {
+                // srcTree.WriteTo(stream);
+                //text = srcTree.ToString();
+                //stream.SetLength(text.Length);
+
+            }
+
+
+            Console.WriteLine("InjectGuidinXMIRootPackage is finished");
+            return 0;
+        }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception occured: {ex.Message}");
+                return -9;
+            }
+        }
+    }
+}
